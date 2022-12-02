86e2904894f6a7f7678636ab7f9bb6c9b9b182a8
Stefan Ober <StefanO@ppedv.de>
2022-12-02 15:35:01 +0100

    Verwendung von .Net Assemblies

---
+++ DotNet/Out-Voice.ps1
@@ -0,0 +1,16 @@
+﻿[cmdletBinding()]
+param(
+[Parameter(Mandatory=$true)]
+[ValidateLength(5,100)]
+[string]$Text
+)
+
+if((Get-Service -Name Audiosrv).Status -ne "Running")
+{
+    Start-Service -Name Audiosrv
+}
+
+Add-Type -AssemblyName System.Speech
+$speaker = New-Object -TypeName System.Speech.Synthesis.SpeechSynthesizer
+
+$speaker.Speak($Text)

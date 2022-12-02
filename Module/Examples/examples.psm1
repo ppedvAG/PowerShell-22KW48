28d027861166a661fd99c87f949fe4727b106479
Stefan Ober <StefanO@ppedv.de>
2022-12-02 14:08:57 +0100

    PipeLine Input und BeginnProcessEnd

---
+++ Module/Examples/examples.psm1
@@ -0,0 +1,39 @@
+﻿function Test-PipeLineInput
+{
+[cmdletBinding()]
+param(
+    [Parameter(ValueFromPipeLine,ValueFromPipeLineByPropertyName)]
+    [string]$Name,
+
+    [Parameter(ValueFromPipeLineByPropertyName)]
+    [string]$Status
+)
+Write-Host -Object "Value From PipeLine [string] $Name"
+Write-Host -Object "Value From PipeLine By PropertyName $Status"
+}
+
+function Test-BeginProcessEnd
+{
+[cmdletBinding()]
+param(
+    [Parameter(ValueFromPipeLine,ValueFromPipeLineByPropertyName)]
+    [string]$Name,
+
+    [Parameter(ValueFromPipeLineByPropertyName)]
+    [string]$Status
+)
+Begin
+{
+    Write-Host -Object "Start" -ForegroundColor Green
+}
+Process
+{
+    Write-Host -Object "Value From PipeLine [string] $Name"
+    Write-Host -Object "Value From PipeLine By PropertyName $Status"
+}
+End
+{
+    Write-Host -Object "Ende" -ForegroundColor Green
+}
+
+}
